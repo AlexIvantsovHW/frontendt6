eff3df6ac13ddd07261fba3e53f1d9f77009654a
Alexander Ivantsov <alexander.ivantsov.hw@gmail.com>
2023-08-17 20:05:54 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   const fetchData= async ()=>{
     try{
       debugger;
-      /* const result=await axios("https://t6-cuzc.onrender.com/user") */
-      const result=await axios("http://localhost:3001/users")
+      const result=await axios("https://t6-cuzc.onrender.com/users")
+      /* const result=await axios("http://localhost:3001/users") */
       debugger;
       setMessage(result);
       console.log(result);
